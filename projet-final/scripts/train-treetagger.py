22dca652de559c0ee418db9e1ebdc50999ea7918
AHMIA Kenza <ahmiakenza@hotmail.com>
2023-12-14 14:44:10 +0100

    script pour avoir input file pout train treetagger

--- projet-final/scripts/train-treetagger.py
+++ projet-final/scripts/train-treetagger.py
@@ -44,3 +44,13 @@ tags = conlluToTag(conllufile)
 with open("../train-treetagger/tags.txt", "w", encoding="utf-8") as f:
     f.write(" ".join(tags))
 
+def conlluToAnnotated(conllufile, outputfile) : 
+    with open(conllufile, "r") as cf, open(outputfile, "w") as of : 
+        for line in cf : 
+            if line != "" and line != "\n":
+                if not line.startswith("#") : 
+                    token = line.split("\t")[1]
+                    tag = line.split("\t")[3]
+                    of.write(token + "\t" + tag + "\n")
+
+#print(conlluToAnnotated("../corpus-lfg/pl_lfg-ud-train.conllu", "../train-treetagger/pl_lfg-ud-train-treetagger.tsv"))
